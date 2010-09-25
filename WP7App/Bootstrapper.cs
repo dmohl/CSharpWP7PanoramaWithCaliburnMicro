7b3b1afacac84038d5bf8ab7aae4e45017fc5b8f
Dan Mohl <dmohl@yahoo.com>
2010-09-25 10:40:18 -0500

    Created

---
+++ WP7App/Bootstrapper.cs
@@ -0,0 +1,43 @@
+﻿// From http://www.codeplex.com/Project/Download/FileDownload.aspx?ProjectName=caliburnmicro&DownloadId=140976
+
+using System;
+using System.Collections.Generic;
+using Microsoft.Phone.Tasks;
+using Caliburn.Micro;
+
+namespace WP7App
+{
+    public class Bootstrapper : PhoneBootstrapper
+    {
+        PhoneContainer container;
+
+        protected override void Configure()
+        {
+            container = new PhoneContainer();
+
+            container.RegisterSingleton(typeof(MainPageViewModel), "MainPageViewModel", typeof(MainPageViewModel));
+            container.RegisterSingleton(typeof(ItemViewModel), "ItemViewModel", typeof(ItemViewModel));
+
+            container.RegisterInstance(typeof(INavigationService), null, new FrameAdapter(RootFrame));
+            container.RegisterInstance(typeof(IPhoneService), null, new PhoneApplicationServiceAdapter(PhoneService));
+
+            container.Activator.InstallChooser<PhoneNumberChooserTask, PhoneNumberResult>();
+            container.Activator.InstallLauncher<EmailComposeTask>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            container.BuildUp(instance);
+        }
+    }
+}
---
+++ WP7App/PhoneContainer.cs
@@ -0,0 +1,22 @@
+﻿// From http://www.codeplex.com/Project/Download/FileDownload.aspx?ProjectName=caliburnmicro&DownloadId=140976
+
+using System;
+using Caliburn.Micro;
+
+namespace WP7App
+{
+    public class PhoneContainer : SimpleContainer
+    {
+        public PhoneContainer()
+        {
+            Activator = new InstanceActivator(type => GetInstance(type, null));
+        }
+
+        public InstanceActivator Activator { get; private set; }
+
+        protected override object ActivateInstance(Type type, object[] args)
+        {
+            return Activator.ActivateInstance(base.ActivateInstance(type, args));
+        }
+    }
+}
